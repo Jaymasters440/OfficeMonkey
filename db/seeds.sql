21450cfe7894e8512533d01f83fa82f1f955abf7
Thor the Fixer <120442441+Jaymasters440@users.noreply.github.com>
2023-03-21 16:30:48 -0800

    Create Seeds and Schema.sql code.

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ("Technical Development"),
+       ("Control Systems"),
+       ("Public Relations"),
+       ("IT"),
+       ("Finance"),
+       ("logistics");
+
+INSERT INTO role (name, department)
+VALUES  ("Loki Laufeyjarson" , 3),
+        ("Thor Odinson", 2),
+        ("Var ", 5),
+        ("Hulk" 2),
+        ("Tony Stark" , 1),
+        ("Bruce Wane" 1),
+        ("Amy Bendix" , 5),
+        ("Leland Owlsley" , 6),
+        ("Natasha Romanoff" , 4)
+        ("Ultron" , 1)
+        ("Loelei" , 4)
