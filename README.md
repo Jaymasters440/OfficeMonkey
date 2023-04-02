f985ecaa6b2bd241e0c62f5851d8902b6bcc43ca
Thor the Fixer <120442441+Jaymasters440@users.noreply.github.com>
2023-04-01 19:42:48 -0800

    add Readme notes.

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # OfficeMonkey
 Program to keep track of Employees titles, roles and salaries.
+# OfficeMonkey [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+  ## Description
+  
+  A platform for busy business people to track employees, roles, salaries and managers and their departments.
+  
+  
+  ## Table of Contents 
+  
+  - [Badges](#badges)
+  - [Contribution](#contribution)
+  - [Installation](#installation)
+  - [License](#license)
+  - [Questions](#questions)
+  - [Usage](#usage)
+  
+  ## Installation
+  
+  Installed using commanline inputs "node install" and make sure source db/schema.sql and source db/seeds.sql have been initialized as well. A vidio tutorial can be seen at 
+  
+  ## Usage
+  
+  Used to help people keep track of, add employees, managers, roles and departments. 
+        
+  ## License
+  
+  For information on our license please follow this link: [Click here to link to License](https://opensource.org/licenses/BSD-3-Clause)
+  
+  ---
+  
+
+  ## Badges
+  
+  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+  [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+  
+  
+  ## Contribution
+  
+  Please don't breake it. Thank you. 
+  
+  
+  ## Questions
+
+  [github.com/jaymasters440](github.com/jaymasters440)
+  
+
+
+
+
+
+  [jaymasters440@gmail.com](jaymasters440@gmail.com)
--- helpers/sqlQueries.js
+++ helpers/sqlQueries.js
@@ -17,104 +17,125 @@ const db = mysql.createConnection(
 //function to display departments with a promise to reject if error or resolve if successful
 
 function displayDepartments() {
-    return new Promise(function(resolve,reject){
-    db.query(`SELECT * FROM department;`, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        console.table(result);
-        resolve();
-        
+    return new Promise(function (resolve, reject) {
+        db.query(`SELECT * FROM department;`, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            console.table(result);
+            resolve();
+
+        });
     });
-});
 
 }
+function updateEmployeeRole(employee,role) {
+    return new Promise(function (resolve, reject) {
+        db.query(`SELECT id FROM role WHERE title = "${role}";`, (err, result) => {
+            if (err) {
+                reject(err);
+            }
 
-function displayRoles() 
-    {return new Promise(function(resolve,reject){
-    db.query(`SELECT role.id, title, department.name AS department, salary FROM role INNER JOIN department ON role.department_id = department.id;`, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        console.table(result);
-        resolve();
-    });
-});
-}
-
-function displayAllEmployees() {
-    return new Promise(function(resolve,reject){
-    db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager.first_name AS manager FROM employee JOIN role ON employee.role_id = role.id LEFT JOIN employee manager ON manager.id = employee.manager_id JOIN department ON role.department_id=department.id;`, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        console.table(result);
-        resolve(result);
-    });
+            var roleId = result[0].id;
+        db.query(`UPDATE employee SET role_id = ${roleId} WHERE CONCAT(first_name, " ", last_name )= "${employee}";`, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+             console.log("Success!");
+            resolve();
+        });
+    }); 
 })
 }
 
-function addEmployee(first_name, last_name, role_name, manager_name) {
-    return new Promise(function(resolve,reject){
-    db.query(`SELECT id FROM role WHERE title = "${role_name}";`, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-            
-        var role_id = result[0].id;
-        db.query(`SELECT id FROM employee WHERE CONCAT (first_name, " ", last_name) = "${manager_name}"; `,(err, result) => {
-            if(err) {
+function displayRoles() {
+    return new Promise(function (resolve, reject) {
+        db.query(`SELECT role.id, title, department.name AS department, salary FROM role INNER JOIN department ON role.department_id = department.id;`, (err, result) => {
+            if (err) {
                 reject(err);
             }
-            
-            var manager_id = result[0].id;
-            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE ("${first_name}", "${last_name}", "${role_id}", "${manager_id}");`, (err, result) => {
+            console.table(result);
+            resolve();
+        });
+    });
+};
+
+function displayAllEmployees() {
+    return new Promise(function (resolve, reject) {
+        db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager.first_name AS manager FROM employee JOIN role ON employee.role_id = role.id LEFT JOIN employee manager ON manager.id = employee.manager_id JOIN department ON role.department_id=department.id;`, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            console.table(result);
+            resolve(result);
+        });
+    })
+};
+// change from 72 to reject from consloe.log
+function addEmployee(first_name, last_name, role_name, manager_name) {
+    return new Promise(function (resolve, reject) {
+        db.query(`SELECT id FROM role WHERE title = "${role_name}";`, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+
+            var role_id = result[0].id;
+            db.query(`SELECT id FROM employee WHERE CONCAT (first_name, " ", last_name) = "${manager_name}"; `, (err, result) => {
                 if (err) {
                     reject(err);
                 }
-                console.log(`Added ${first_name} ${last_name} to employee`)
-                resolve();
-            })
-        })
 
-        })})
-   
+                var manager_id = result[0].id;
+                db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE ("${first_name}", "${last_name}", "${role_id}", "${manager_id}");`, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    console.log(`Added ${first_name} ${last_name} to employee`)
+                    resolve();
+                })
+            })
+
+        })
+    })
+
 
 }
 function addDepartment(name) {
-    return new Promise(function(resolve,reject){
-    db.query(`INSERT INTO department (name) VALUE ("${name}");`, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        resolve(`Added ${title} to department`);
-    })})
+    return new Promise(function (resolve, reject) {
+        db.query(`INSERT INTO department (name) VALUE ("${name}");`, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(`Added ${name} to department`);
+        })
+    })
 }
 
 
 
 function addRole(title, salary, department) {
 
-    return new Promise(function(resolve,reject){
+    return new Promise(function (resolve, reject) {
         db.query(`SELECT id FROM department WHERE name = "${department}";`, (err, result) => {
             if (err) {
                 reject(err);
             }
-                
-            var department_id = result[0].id;
-                      
-                
-                    db.query(`INSERT INTO role (title, salary, department_id) VALUE ("${title}", "${salary}", "${department_id}");`, (err, result) => {
-                    if (err) {
-                        reject(err);
-                    }
-                    console.log(`Added ${title} to roles`)
-                    resolve();
-                })
-    
-            })})
 
-    }
+            var department_id = result[0].id;
+
+
+            db.query(`INSERT INTO role (title, salary, department_id) VALUE ("${title}", "${salary}", "${department_id}");`, (err, result) => {
+                if (err) {
+                    reject(err);
+                }
+                console.log(`Added ${title} to roles`)
+                resolve();
+            })
+
+        })
+    })
+
+}
 
 /*function getDepartmentNames() {
     db.query(`SELECT name FROM department;`, (err, result) => {
@@ -126,57 +147,51 @@ function addRole(title, salary, department) {
 */
 
 function getRoleNames() {
-    return new Promise(function(resolve,reject){
-        db.query({sql: `SELECT JSON_ARRAYAGG(title) as title FROM role;`, rowsAsArray: true}, (err, result) => {
+    return new Promise(function (resolve, reject) {
+        db.query({ sql: `SELECT JSON_ARRAYAGG(title) as title FROM role;`, rowsAsArray: true }, (err, result) => {
             if (err) {
                 reject(err);
             }
             var roles = [];
             result[0][0].forEach(role => roles.push(role))
             //.log(roles)
-            resolve (roles);
+            resolve(roles);
         });
     })
-   
+
 }
 
 function getEmployeeNames() {
-    return new Promise(function(resolve,reject){
-        db.query({sql: `SELECT JSON_ARRAYAGG(CONCAT(first_name, " ", last_name)) as name FROM employee;`, rowsAsArray: true}, (err, result) => {
+    return new Promise(function (resolve, reject) {
+        db.query({ sql: `SELECT JSON_ARRAYAGG(CONCAT(first_name, " ", last_name)) as name FROM employee;`, rowsAsArray: true }, (err, result) => {
             if (err) {
                 reject(err);
             }
             var roles = [];
             result[0][0].forEach(role => roles.push(role))
             //.log(roles)
-            resolve (roles);
+            resolve(roles);
         });
-    })
-   
+    });
+
 }
 
 function getDepartmentNames() {
-    return new Promise(function(resolve,reject){
-        db.query({sql: `SELECT JSON_ARRAYAGG(name) as name FROM department;`, rowsAsArray: true}, (err, result) => {
+    return new Promise(function (resolve, reject) {
+        db.query({ sql: `SELECT JSON_ARRAYAGG(name) as name FROM department;`, rowsAsArray: true }, (err, result) => {
             if (err) {
                 reject(err);
             }
             var roles = [];
             result[0][0].forEach(role => roles.push(role))
             //.log(roles)
-            resolve (roles);
+            resolve(roles);
         });
     })
-   
+
 }
-/*function getEmployeeNames() {
-    db.query(`SELECT first_name, last_name FROM employee`, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        return (result);
-    });
-}*/
+
+
 
 module.exports = {
     displayRoles,
@@ -188,4 +203,5 @@ module.exports = {
     addDepartment,
     addRole,
     addEmployee,
+    updateEmployeeRole,
 }
